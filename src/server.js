a47da0f59884e6228ba7183e000f8971fe19b267
Paula Rodrigues <pullynnhah@icloud.com>
2023-04-29 13:13:11 -0300

    feat: create template routes in server.js

--- src/server.js
+++ src/server.js
@@ -4,8 +4,26 @@ import "dotenv/config";
 
 const server = express();
 const port = process.env.PORT || 5000;
+const usersDB = [];
+const tweetsDB = [];
 
 server.use(cors());
 server.use(json());
+
+server.post("/sign-up", (req, res) => {
+  res.send("POST /sign-up");
+});
+
+server.post("/tweets", (req, res) => {
+  res.send("POST /tweets");
+});
+
+server.get("/tweets", (req, res) => {
+  res.send("GET /tweets");
+});
+
+server.get("/tweets/:username", (req, res) => {
+  res.send("GET /tweets/:username");
+});
 
 server.listen(port, () => console.log(`🎉 Server listening on port ${port}...`));
